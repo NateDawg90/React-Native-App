51e2c866d805e8d82cd22baa296404c4b7671e5f
Nathan Johnson <rnjclemson@gmail.com>
2019-02-07 02:16:02 -0600

    create pick image, pick location; outsource buttons and heading text; style Auth screen and share place screen

--- src/components/PlaceInput/PlaceInput.js
+++ src/components/PlaceInput/PlaceInput.js
@@ -1,53 +1,17 @@
 import React, {Component} from 'react';
 import {View, TextInput, StyleSheet, Button} from 'react-native';
+import DefaultInput from '../UI/DefaultInput/DefaultInput';
 
-type Props = {
-  onPlaceAdded: Function
-};
-class PlaceInput extends Component<Props> {
-  state = {
-    placeName: ""
-  };
+const PlaceInput = props => (
+  <DefaultInput 
+    placeholder='Place Name' 
+    value={props.placeName} 
+    onChangeText={props.onChangeText}
+  />
+)
 
-  placeNameChangedHandler = val => {
-    this.setState({
-      placeName: val
-    });
-  }
-
-  placeAddedHandler = (place) => {
-    if (this.state.placeName.trim() === '') {
-      return;
-    }
-    this.props.onPlaceAdded(this.state.placeName);
-  }
-
-  render() {
-    return (
-      <View style={styles.inputContainer}>
-        <TextInput 
-          style={styles.placeInput} 
-          placeholder="enter an awesome place"
-          value={this.state.placeName} 
-          onChangeText={this.placeNameChangedHandler} />
-        <Button style={styles.placeButton} title="Add" onPress={this.placeAddedHandler} />
-      </View>
-    );
-  }
-};
 
 const styles = StyleSheet.create({
-  inputContainer: {
-    width: '100%',
-    flexDirection: 'row',
-    justifyContent: 'space-between'
-  },
-  placeInput: {
-    width: '70%',
-  },
-  placeButton: {
-    width: '30%'
-  },
 });
 
 export default PlaceInput; 
