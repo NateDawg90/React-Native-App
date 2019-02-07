d0f30acc68e540eacbd8f6818985c02528a1434f
Nathan Johnson <rnjclemson@gmail.com>
2019-02-06 20:29:12 -0600

    create share place and find place tabs; add side nav menu; create input component

--- src/screens/SharePlace/SharePlace.js
+++ src/screens/SharePlace/SharePlace.js
@@ -5,6 +5,21 @@ import {addPlace} from '../../store/actions/index';
 import PlaceInput from '../../components/PlaceInput/PlaceInput';
 
 class SharePlaceScreen extends Component {
+  constructor(props) {
+    super(props);
+    this.props.navigator.setOnNavigatorEvent(this.onNavigatorEvent);
+  }
+
+  onNavigatorEvent = e => {
+    if (e.type === 'NavBarButtonPress') {
+      if (e.id === 'sideDrawerToggle') {
+        this.props.navigator.toggleDrawer({
+          side: 'left',
+
+        })
+      }
+    }
+  }
   placeAddedHandler = name => {
     this.props.addPlace(name);
   }
