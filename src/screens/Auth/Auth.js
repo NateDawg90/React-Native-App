2a1f6bedd1f75e1776f61547508519cf98d83b05
Nathan Johnson <rnjclemson@gmail.com>
2019-02-09 01:41:24 -0600

    add styling to auth page; add responsiveness; add animations to find place

--- src/screens/Auth/Auth.js
+++ src/screens/Auth/Auth.js
@@ -1,5 +1,5 @@
 import React, { Component} from "react";
-import { View, Text, Button, StyleSheet, ImageBackground } from 'react-native';
+import { View, Text, Button, StyleSheet, ImageBackground, Dimensions } from 'react-native';
 
 import startMainTabs from "../MainTabs/StartMainTabs";
 import DefaultInput from "../../components/UI/DefaultInput/DefaultInput";
@@ -9,6 +9,24 @@ import ButtonWithBackground from "../../components/UI/Button/ButtonWithBackgroun
 import backgroundImage from '../../assets/background.jpg';
 
 class AuthScreen extends Component {
+  state = {
+    viewMode: Dimensions.get('window').height > 500 ? 'portrait' : 'landscape'
+  }
+  constructor(props) {
+    super(props);
+    Dimensions.addEventListener('change', this.updateStyles);
+  }
+  
+  componentWillUnmount() {
+    Dimensions.removeEventListener('change', this.updateStyles);
+  }
+
+  updateStyles = dims => {
+    this.setState({
+      viewMode: dims.window.height > 500 ? 'portrait' : 'landscape'
+    })
+  }
+
   loginHandler = () => {
     startMainTabs();
   }
@@ -17,14 +35,42 @@ class AuthScreen extends Component {
     return (
       <ImageBackground style={styles.backgroundImage} source={backgroundImage}>
         <View style={styles.container}>
+        {this.state.viewMode === 'portrait' && 
           <MainText>
             <HeadingText>Please Log in</HeadingText>
           </MainText>
+        
+        }
           <ButtonWithBackground color='#29aaf4' >Switch to Login</ButtonWithBackground>
           <View style={styles.inputContainer}>
+
             <DefaultInput placeholder="Your email address" style={styles.input} />
-            <DefaultInput placeholder="Password" style={styles.input}/>
-            <DefaultInput placeholder="Confirm Password" style={styles.input}/>
+            <View 
+              style={
+                this.state.viewMode === 'portrait' 
+                ? styles.portraitPasswordContainer 
+                : styles.landscapePasswordContainer
+              }
+            >
+              <View 
+                style={
+                  this.state.viewMode === 'portrait' 
+                  ? styles.portraitPasswordWrapper
+                  : styles.landscapePasswordWrapper
+                }
+              >
+                <DefaultInput placeholder="Password" style={styles.input}/>
+              </View>
+              <View
+                style={
+                  this.state.viewMode === 'portrait' 
+                  ? styles.portraitPasswordWrapper
+                  : styles.landscapePasswordWrapper
+                }
+              >
+                <DefaultInput placeholder="Confirm Password" style={styles.input}/>
+              </View>
+            </View>
           </View>
           <ButtonWithBackground color='#29aaf4' onPress={this.loginHandler} >Submit</ButtonWithBackground>
         </View>
@@ -53,6 +99,21 @@ const styles = StyleSheet.create({
     width: '100%',
     flex: 1
   },
+  landscapePasswordContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  },
+  portraitPasswordContainer: {
+    flexDirection: 'column',
+    justifyContent: 'flex-start'
+  },
+  landscapePasswordWrapper: {
+    width: '45%'
+  },
+  portraitPasswordWrapper: {
+    width: '100%'
+  }
+
 })
 
 export default AuthScreen;
--- src/screens/SharePlace/SharePlace.js
+++ src/screens/SharePlace/SharePlace.js
@@ -9,6 +9,10 @@ import MainText from '../../components/UI/MainText/MainText';
 import HeadingText from '../../components/UI/HeadingText/HeadingText';
 
 class SharePlaceScreen extends Component {
+  static navigatorStyle = {
+    navBarButtonColor: 'orange'
+  }
+  
   state = {
     placeName: ''
   }
